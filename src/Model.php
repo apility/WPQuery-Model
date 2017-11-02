c278b0fb82f2ce9e062a467fe0c4d98878d4a0f4
thomas-alrek <thomas@alrek.no>
2017-11-02 09:44:32 +0100

    Implement save and create

--- src/Model.php
+++ src/Model.php
@@ -14,7 +14,9 @@ abstract class Model extends \Jenssegers\Model\Model
     protected $apiKey;
     protected $guzzle;
     protected $hidden = [];
+    protected $modified = [];
     protected $attributes = [];
+    protected $primaryKey = 'ID';
 
     public function __construct(array $attributes = [])
     {
@@ -23,7 +25,7 @@ abstract class Model extends \Jenssegers\Model\Model
         if (is_null($this->apiKey) || empty($this->apiKey)) {
             throw new Exception('No API key supplied');
         }
-        $this->host = rtrim($this->host, '/') . '/';
+        $this->host = rtrim(env('WPQUERY_HOST'), '/') . '/';
         $this->host .= 'wp-json/wpquery/v1/query/' . $this->table . '/';
         $this->guzzle = new Client([
             'base_uri' => $this->host
@@ -71,7 +73,7 @@ abstract class Model extends \Jenssegers\Model\Model
                 3600,
                 function () use ($id, $model) {
                     $response = $model->guzzle->get($id . '?apikey=' . $model->apiKey)->getBody();
-                    return new static(json_decode($response, true));
+                    return new static(json_decode($response, true), $id);
                 }
             );
         } catch (Exception $ex) {
@@ -87,15 +89,76 @@ abstract class Model extends \Jenssegers\Model\Model
             }
             return $this->attributes[$prop];
         }
+        if (method_exists($this, 'get' . $prop . 'attribute')) {
+            if (isset($this->attributes[$prop])) {
+                return $this->{'get' . $prop . 'attribute'}($this->attributes[$prop]);
+            }
+            return $this->{'get' . $prop . 'attribute'}();
+        }
         trigger_error(
             'Undefined property: ' . get_class($this) . '::$' . $prop,
             E_USER_NOTICE
         );
     }
     
-    public function __set($prop, $val)
+    public function __set($prop, $value)
     {
-        // Immutable
+        if ($prop !== $this->primaryKey) {
+            if (isset($this->attributes[$prop])) {
+                if ($value !== $this->attributes[$prop]) {
+                    $this->attributes[$prop] = $value;
+                    if (method_exists($this, 'set' . $prop . 'attribute')) {
+                        $this->{'set' . $prop . 'attribute'}($value);
+                    }
+                    $this->modified[] = $prop;
+                    $this->modified = array_unique($this->modified);
+                }
+            } else {
+                if (method_exists($this, 'set' . $prop . 'attribute')) {
+                    $this->{'set' . $prop . 'attribute'}($value);
+                } else {
+                    $this->attributes[$prop] = $value;
+                    $this->modified[] = $prop;
+                }
+            }
+        }
+    }
+
+    public function save()
+    {
+        if (!empty($this->modified)) {
+            try {
+                $modified = [];
+                foreach ($this->modified as $key) {
+                    if ($key !== $this->primaryKey) {
+                        $modified[$key] = $this->attributes[$key];
+                    }
+                }
+                if (isset($this->attributes[$this->primaryKey])) {
+                    $this->guzzle->put(
+                        $this->attributes[$this->primaryKey] . '?apikey=' . $this->apiKey,
+                        ['json' => $modified]
+                    );
+                    Cache::forget('__wpquery__' . '/' . $this->host . '__all__');
+                    Cache::forget('__wpquery__' . '/' . $this->host . $this->attributes[$this->primaryKey]);
+                } else {
+                    $result = json_decode($this->guzzle->post(
+                        '?apikey=' . $this->apiKey,
+                        ['json' => $modified]
+                    )->getBody());
+                    $this->attributes[$this->primaryKey] = $result->id;
+                }
+                $attributes = json_decode(
+                    $this->guzzle->get(
+                        $this->attributes[$this->primaryKey] . '?apikey=' . $this->apiKey
+                )->getBody(), true);
+                $this->modified = [];
+                return true;
+            } catch (Exception $ex) {
+                throw new Exception('Unable to save. Is write support enabled?');
+            }
+        }
+        return false;
     }
 
     public static function first()
